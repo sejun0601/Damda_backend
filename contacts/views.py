d18db86c3e21560fdaccf8bf1aa8dec7ced417c0
sejun <sejun0601@naver.com>
2025-01-01 21:32:08 +0900

    contacts model added

--- contacts/views.py
+++ contacts/views.py
@@ -10,15 +10,11 @@ import logging
 logger = logging.getLogger(__name__)
 
 class ContactCreateView(APIView):
-    parser_classes = [MultiPartParser, FormParser]
+    parser_classes = [MultiPartParser, FormParser]  # 파일 처리
 
     def post(self, request, *args, **kwargs):
-        print("Request data:", request.data)  # 폼 데이터 출력
-        print("Request files:", request.FILES)  # 파일 데이터 출력
-
         serializer = ContactSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        print("Serializer errors:", serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
