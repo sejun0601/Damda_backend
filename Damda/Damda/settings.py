26dae6c28ca25f36c7a7d2d72b0a0fc748177d5c
sejun <sejun0601@naver.com>
2025-01-01 16:40:02 +0900

    django project

--- Damda/Damda/settings.py
+++ Damda/Damda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uvh0lj=u^y6pb_fw-)8_x46$#q77p_$wjq6c8kv4l@acf(_su8'
+SECRET_KEY = 'django-insecure-8&#6jljwrbff*#q+%@_c+_e)7hri_koi$4m^0exkb@qpid663%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
